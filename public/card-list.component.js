c5b700633d29e8069c20a186ba3c8d8708ac0a70
Chowdhurian <manil.chowdhury@gmail.com>
2016-12-08 11:12:59 -0800

    🎨 Make app use json data

--- public/card-list.component.js
+++ public/card-list.component.js
@@ -1,35 +1,21 @@
 angular
+
 // on the `cardList` module
   .module('cardList')
+
   // register the `cardList` component
   .component('cardList', {
     // the url will be relative to `index.html`
     templateUrl: './card-list.template.html',
-    controller: function CardListController() {
-      this.cards = [
-        {
-          "firstname":"Steven",
-          "lastname":"Zozula",
-          "phone":"6045200192",
-          "address":"100 Water St., Vancouver, BC, V8I 2G3",
-          "email":"steven@changeheroes.com"
-        },
-        {
-          "firstname":"Daryl",
-          "lastname":"Chmyko",
-          "phone":"6042240422",
-          "address":"289 Cordova St., Vancouver, BC, V6B 2L4",
-          "email":"daryl@changeheroes.com"
-        },
-        {
-          "firstname":"Aaron",
-          "lastname":"Vandenbrink",
-          "phone":"6047581029",
-          "address":"200 Abbott St., Vancouver, BC, V9M 3K2",
-          "email":"aaron.vandenbrink@gmail.com"
-        }
-      ]
-      
-      this.orderProp = 'firstname'
-    }
+    controller: ['$http',
+      function CardListController($http) {
+        var self = this
+        this.orderProp = 'firstname'
+
+        $http.get('assets/contacts.json')
+          .then(function(response) {
+            self.cards = response.data
+          })
+      }
+    ]
   })
