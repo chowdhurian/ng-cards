32a0e3ac7b8ef2e9b792591de19e458ba8494a2c
Chowdhurian <manil.chowdhury@gmail.com>
2016-12-09 16:53:52 -0800

    ⬆️ Add ngRoute

--- public/app.module.js
+++ public/app.module.js
@@ -1,8 +1,22 @@
 'use strict'
 
 // define the `contactList` module
-angular.module('contactList', [
-  // which depends on the `cardList` and `cardDetail` modules
-  'cardList',
-  'cardDetail'
-])
+angular
+  .module('contactList', [
+    // which depends on the `cardList` and `cardDetail` modules
+    'ngRoute',
+    'cardList',
+    // 'cardSearch',
+    'cardDetail'
+  ])
+  .factory('Messages', function() {
+    var messages = {}
+
+    messages.list = []
+
+    messages.add = function(message) {
+      messsages.list.push({id: messages.list.length, text: message})
+    }
+
+    return messages
+  })
